971ccd53507603ee8d0d12dd703fbf23209ddf3b
Bereket Issack <bereketissack@gmail.com>
2025-07-27 03:30:17 -0700

    Update app.py

--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -136,5 +136,5 @@ If the user is engaging in discussion, try to steer them towards getting in touc
 
 if __name__ == "__main__":
     me = Me()
-    gr.ChatInterface(me.chat, type="messages").launch(share=True)
+    gr.ChatInterface(me.chat, type="messages").launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
     
